ab0f5bbb9fc4d87c270cd53b3f23f0d9ef3b4f6f
Baxrom <92705188+Baxromumarov@users.noreply.github.com>
2022-03-18 09:15:30 +0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -20,7 +20,7 @@ pygame.display.set_caption('Snake Game by Baxrom')
 clock = pygame.time.Clock()
  
 snake_block = 10
-snake_speed = 15
+snake_speed = 10
  
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
